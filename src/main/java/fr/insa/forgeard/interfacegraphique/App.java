9ae35d884d28d31688567b33ca20253aca6315bc
artus <artus@172.17.53.192>
2022-05-19 01:22:22 +0200

    interface graphique suite J'ai rajouté le layout de base, on peut maintenant créer des points de différentes couleurs en fct de leurs type en remplissant leurs coordonnées et leurs type dans l'interface.

--- src/main/java/fr/insa/forgeard/interfacegraphique/App.java
+++ src/main/java/fr/insa/forgeard/interfacegraphique/App.java
@@ -18,60 +18,135 @@ import fr.insa.forgeard.treillis.Vecteur2D;
 import javafx.geometry.Insets;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 
 /**
  * JavaFX App
  */
 public class App extends Application {
+    private int xpos;
+    private int ypos;
 
     
     @Override
     public void start(Stage stage) throws IOException {
         Treillis treillis = new Treillis();
-        VBox root = new VBox();
+        
+        //initialisation borderpane
+        BorderPane root = new BorderPane();
+        
+        //créer les boites
+        VBox pointscreate = new VBox();
+        HBox coordonneespts = new HBox();
+        HBox hbox1 = new HBox();
+        
+        //création canvas
+        Canvas canvas = new Canvas();
+        canvas.setHeight(700);
+        canvas.setWidth(1000);
+        
+        //Zone graphique
+        GraphicsContext graphics_context = 
+            canvas.getGraphicsContext2D();
+        graphics_context.setFill(Color.LIGHTGREY);
+        graphics_context.fillRect(0, 0, 1000, 700);
+        
+        Group group = new Group(canvas);
+        root.setCenter(group);
+        
+        //Onglet création de points
+        Label barredecla = new Label("Création de point :");
+        pointscreate.getChildren().add(barredecla);
+        
+        Label typepoint = new Label("Type de point :");
+        hbox1.getChildren().add(typepoint);
+        
+        
+        ComboBox comboBox = new ComboBox();
+        comboBox.getItems().add("Noeud Double");
+        comboBox.getItems().add("Noeud Simple");
+        comboBox.getItems().add("Noeud Appui");
+        hbox1.getChildren().add(comboBox);
+        
+        Label x = new Label("X:");
+        coordonneespts.getChildren().add(x);
+        
+        TextField xinput = new TextField();
+        xinput.setPrefWidth(30);
+        xinput.setPrefHeight(8);
+        coordonneespts.getChildren().add(xinput);
+        
+        Label y= new Label("Y:");
+        coordonneespts.getChildren().add(y);
+        
+        TextField yinput = new TextField();
+        yinput.setPrefWidth(30);
+        yinput.setPrefHeight(8);
+        coordonneespts.getChildren().add(yinput);
+        pointscreate.getChildren().add(coordonneespts);
+        pointscreate.getChildren().add(hbox1);
+        // Button créer un point
+        Button button1= new Button("Créer !");
+        pointscreate.getChildren().add(button1);
+        button1.setOnAction(new EventHandler<ActionEvent>(){
+  
+    public void handle(ActionEvent event){
+        xpos = Integer.parseInt(xinput.getText());
+        ypos = Integer.parseInt(yinput.getText());
+        System.out.println("Position x :" + xpos);
+        System.out.println("Position y :" + ypos);
+        System.out.println(comboBox.getValue());
+        
+        //Si noeud double, alors noeud en 
+        if (comboBox.getValue() == "Noeud Double")
+            graphics_context.setFill(Color.RED);
+        graphics_context.fillOval(xpos-1, ypos-1, 5, 5 );
+        if (comboBox.getValue() == "Noeud Simple")
+            graphics_context.setFill(Color.BLUE);
+        graphics_context.fillOval(xpos-1, ypos-1, 5, 5 );
+        if (comboBox.getValue() == "Noeud Appui")
+            graphics_context.setFill(Color.GREEN);
+        graphics_context.fillOval(xpos-1, ypos-1, 5, 5 );
+        
+    
+}
+});
        
-        root.setSpacing(10);
-        root.setPadding(new Insets(15,20, 10,10));
-       
-        // Button 1
-        Button button1= new Button("Button1");
-        root.getChildren().add(button1);
-       
-       
-        // Button 2
-        Button button2 = new Button("Button2");
-        button2.setPrefSize(100, 100);
-        root.getChildren().add(button2);
-       
-        // TextField
-        TextField textField = new TextField("Text Field");
-        textField.setPrefWidth(110);
-         
-       
-        root.getChildren().add(textField);
-       
-        // CheckBox
-        CheckBox checkBox = new CheckBox("Check Box");
-         
-        root.getChildren().add(checkBox);
-       
-        // RadioButton
-        RadioButton radioButton = new RadioButton("Radio Button");
-        root.getChildren().add(radioButton);
-        Scene sc = new Scene(root,550,250);
+        
+        //position bordepane
+        root.setLeft(pointscreate);
+        //root.setLeft(coordonneespts);
+        
+        //canvas
+  
+        // set fill for rectangle
+        graphics_context.setFill(Color.LIGHTGREY);
+        graphics_context.fillRect(0, 0, 1000, 700);
+        
+        root.setCenter(group);
+        // fin test
+        
+        
+        stage.getIcons().add(new Image("https://www.europodiumshop.com/wp-content/uploads/2017/03/G3_EPL_zoom.jpg"));
+        Scene sc = new Scene(root,1200,700);
         stage.setScene(sc);
-        stage.setTitle("Notest");
+        stage.setTitle("Application Treillis");
         stage.show();
     }
 
@@ -79,5 +154,8 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+    
+
 
 }
